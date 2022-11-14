10cc0914af1c63a6aacc8f13586c74010b5fc952
matiasnaakka <matias5230@gmail.com>
2022-11-14 20:32:45 +0200

    middleware

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 'use strict';
 const express = require('express');
+const cors = require('cors');
+const catRoute = require('./routes/catRoute');
+const userRoute = require('./routes/userRoute');
+const {httpError} = require('./utils/errors');
 const app = express();
 const port = 3000;
 
-app.get('/cat/', (req, res) => {
-    res.send('From this endpoint you can get cats.')
+app.use(cors());
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
+app.use(express.static('uploads'));
+
+app.use('/cat', catRoute);
+app.use('/user', userRoute);
+
+app.use((req, res, next) => {
+    const err = httpError('Not found', 404);
+    next(err);
 });
 
-app.get('/cat/:id', function (req, res) {
-    res.send(`From this endpoint you can get cats.${req.params.id}`)
-});
-
-app.post('/cat', (req, res) => {
-    res.send('From this endpoint you can get Hoes.')
-});
-
-app.put('/cat', (req, res) => {
-    res.send('From this endpoint you can get Money.')
-});
-
-app.delete('/cat', (req, res) => {
-    res.send('From this endpoint you can get Drugs.')
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).
+    json({message: err.message || 'Internal server error'});
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
